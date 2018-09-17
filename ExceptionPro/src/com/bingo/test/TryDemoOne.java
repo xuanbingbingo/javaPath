154af44f345b37238c624135a29d59ef35fabe4d
xuanbingbingo <838300414@qq.comm>
2018-09-17 08:11:08 +0800

    '开始工具类'

---
+++ ExceptionPro/src/com/bingo/test/TryDemoOne.java
@@ -0,0 +1,31 @@
+package com.bingo.test;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class TryDemoOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		int one = 12;
+//		int two = 0;
+//		System.out.print("one,two相除结果是："+one/two);
+		
+		Scanner input = new Scanner(System.in);
+		try{
+			int one = input.nextInt();
+			int two = input.nextInt();
+			System.out.println("one,two相除结果是："+one/two);
+		}catch(InputMismatchException e){
+			e.printStackTrace();
+			System.out.println("程序出错了,输入不能为整数");
+		}catch(Exception e){
+			e.printStackTrace();
+			System.out.println("程序出错了！");
+		}finally{
+			System.out.println("无论如何都会执行这句代码");
+		}
+		
+	}
+
+}
